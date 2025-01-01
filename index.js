06f14ed23e430fcf8c3fc1e5e1406acf5449cb00
Rishit <rishitbutola10@gmail.com>
2025-01-01 14:24:41 +0530

    base

---
+++ index.js
@@ -0,0 +1,12 @@
+let menuActive = false;
+
+const hamMenu = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('.menu');
+
+hamMenu.addEventListener('click', () => {
+    menuActive = !menuActive;
+    hamMenu.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+
+
